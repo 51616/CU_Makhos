d18975a0defec65e58e651842293a6197cfe2cd5
Unknown <omoplus_3@hotmail.com>
2019-03-03 15:10:01 +0700

    self

--- Coach_th_checkers.py
+++ Coach_th_checkers.py
@@ -319,7 +319,7 @@ class Coach():
             print('Win count:', self.win_count, 'Loss count:',
                   self.loss_count, 'Draw count:', self.draw_count)
            # print('Draw Count:', self.draw_count)
-            if draw_count < 50:
+            if self.draw_count < 50:
                 self.checkpoint_iter = i
             else:
                 print('Too much draw, no checkpoint created')
