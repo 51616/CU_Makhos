457df71e2bd45ae4671bf85ebf3544dc5bb67824
Unknown <omoplus_3@hotmail.com>
2019-03-12 18:34:51 +0700

    fix mcts

--- MCTS_th_checkers.py
+++ MCTS_th_checkers.py
@@ -96,18 +96,20 @@ class MCTS():
         if s not in self.Ps:
             # leaf node
             valids = self.game.getValidMoves(canonicalBoard, 1)
+            pi, v = self.nnet.predict(
+                boardHistory, self.game.gameState.turn, self.game.gameState.stale, valids)
             if np.sum(valids) == 1:
                 self.Ps[s] = np.argmax(valids)
+
+            elif is_search_root and not self.eval:
+                dir_noise = np.random.dirichlet([1]*32*32)
+                self.Ps[s] = 0.75*pi + 0.25*dir_noise
+                # #pi += EPS
+                # dir_noise = np.random.dirichlet([1]*32*32)
+                # self.Ps[s] = 0.75*pi + 0.25*dir_noise
+                # #self.Ps[s] = [p if p > 1e-8 else 0 for p in prob]
             else:
-                pi, v = self.nnet.predict(
-                    boardHistory, self.game.gameState.turn, self.game.gameState.stale, valids)
-                if self.eval:
-                    self.Ps[s] = pi
-                elif is_search_root:
-                    #pi += EPS
-                    dir_noise = np.random.dirichlet([1]*32*32)
-                    self.Ps[s] = 0.75*pi + 0.25*dir_noise
-                    #self.Ps[s] = [p if p > 1e-8 else 0 for p in prob]
+                self.Ps[s] = pi
 
             # for p, i in enumerate(self.Ps[s]):
             #     if p < 1e-5:
