2015f255c81be8569debcd325b88b2279913ef31
Unknown <omoplus_3@hotmail.com>
2019-03-12 17:17:32 +0700

    400 search

--- pit_th_checkers.py
+++ pit_th_checkers.py
@@ -117,7 +117,7 @@ if __name__ == "__main__":
     n1 = NNet(g, gpu_num=0)
     n1.load_checkpoint('/workspace/CU_Makhos/models/',
                        'train_iter_190.pth.tar')
-    args1 = dotdict({'numMCTSSims': 200, 'cpuct': 0.0})
+    args1 = dotdict({'numMCTSSims': 400, 'cpuct': 1.0})
     mcts1 = MCTS(g, n1, args1, eval=True)
     def n1p(x): return np.random.choice(
         32*32, p=mcts1.getActionProb(x, temp=0))
