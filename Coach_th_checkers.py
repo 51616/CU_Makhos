0e67e72657f45bd4ffe1ad4d2522a9c9769ae07b
Unknown <omoplus_3@hotmail.com>
2019-03-13 01:58:20 +0700

    fix weights sum

--- Coach_th_checkers.py
+++ Coach_th_checkers.py
@@ -129,7 +129,7 @@ def AsyncMinimaxPlay(game, args):
         trainExamples.append([canonicalBoard, curPlayer, pi,
                               game.gameState.turn, game.gameState.stale, valids])
 
-        action = np.random.choice(len(pi), p=pi)
+        action = random.choices(np.arange(0, len(pi)), weights=pi)
         board, curPlayer = game.getNextState(
             board, curPlayer, action)
 
